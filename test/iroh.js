2ec9e5747890961510c0f348a8130d1a22c0aea9
lexik04 <akronowi@andrew.cmu.edu>
2024-10-22 22:47:03 -0400

    added Iroh repo

--- test/iroh.js
+++ test/iroh.js
@@ -1,6 +1,15 @@
-function factorial(n) {
-    if (n === 0) return 1;
-    return n * factorial(n - 1);
-  };
-factorial(3);
+let code = `let foo = 42`;
+let stage = new Iroh.Stage(code);
+
+// create a listener
+let listener = stage.addListener(Iroh.VAR);
+// jump in *after* the variable got created
+listener.on("after", (e) => {
+  // this logs the variable's 'name' and 'value'
+  console.log(e.name, "=>", e.value); // prints "foo => 42"
+});
+
+// the stage object holds the patched version of our code
+// we just use eval here to keep things simple
+eval(stage.script);
 
