f098cfffb1f0ed5fb985b3a36a9bc8a02a66b0a0
lexik04 <akronowi@andrew.cmu.edu>
2024-10-22 23:26:48 -0400

    ran example code given in the iroh repo and saw expected output

--- test/iroh.js
+++ test/iroh.js
@@ -1,15 +1,38 @@
-let code = `let foo = 42`;
+//example on code visualization in the Iroh repo in the GETTING_STARTED.md
+const Iroh = require('iroh');
+
+
+let code = `
+  if (true) {
+    for (ii = 0; ii < 3; ++ii) {
+      let a = ii * 2;
+    };
+  }
+`;
+
 let stage = new Iroh.Stage(code);
 
-// create a listener
-let listener = stage.addListener(Iroh.VAR);
-// jump in *after* the variable got created
-listener.on("after", (e) => {
-  // this logs the variable's 'name' and 'value'
-  console.log(e.name, "=>", e.value); // prints "foo => 42"
+// while, for etc.
+stage.addListener(Iroh.LOOP)
+.on("enter", function(e) {
+  // when we enter the loop
+  console.log(" ".repeat(e.indent) + "loop enter");
+})
+.on("leave", function(e) {
+  // when we leave the loop
+  console.log(" ".repeat(e.indent) + "loop leave");
 });
 
-// the stage object holds the patched version of our code
-// we just use eval here to keep things simple
+// if, else if
+stage.addListener(Iroh.IF)
+.on("enter", function(e) {
+  // when we enter the if
+  console.log(" ".repeat(e.indent) + "if enter");
+})
+.on("leave", function(e) {
+  // when we leave the if
+  console.log(" ".repeat(e.indent) + "if leave");
+});
+
 eval(stage.script);
 
