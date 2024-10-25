dea00e255132f2e36f0b873060ae81e951daa25b
Victoria Solsky <vsolsky@andrew.cmu.edu>
2024-10-24 20:49:45 -0400

    installation of JScent and test file

---
+++ test/jscent.js
@@ -0,0 +1,94 @@
+//dead code
+const dead = require('dead');
+
+//long parameter
+function par(a, b, c, d, e) {
+    return a
+}
+
+//long parameter
+function longMeth(a, b) {
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b
+    a=b    
+    a=b
+    a=b
+    a=b    
+    a=b
+    return a
+}
+//long message chain
+function chain(a, b, c, d, top){
+    for (let x = a; x <= b; x++) {
+        for (let y = c; y <= d; y++) {
+            this.game_grid[x][y]["top"] = top;
+        }
+    }
+}
+
+//Method comments
+function comments(a){
+    //comment
+    //comment
+    //comment
+    //comment
+    //comment
+    //comment
+    return a
+}
