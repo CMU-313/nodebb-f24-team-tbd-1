5a48033e76a480f45cb1b18768ef56b484a48a29
Andrew Youn <52907065+ay0503@users.noreply.github.com>
2024-11-15 03:44:04 -0500

    Fixed bugs.

--- src/translate/index.js
+++ src/translate/index.js
@@ -6,7 +6,9 @@ const translatorApi = module.exports;
 translatorApi.translate = async function (postData) {
     // Edit the translator URL below
     const TRANSLATOR_API = "https://nodebb-f24-translator.azurewebsites.net"
-    const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
+    const query = `?content=${postData.content}`;
+    const url = encodeURI(TRANSLATOR_API + '/' + query);
+    const response = await fetch(url);
     const data = await response.json();
     return [data["is_english"], data["translated_content"]]
 }
